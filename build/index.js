05fd4e2a0153b2e247f7186f18dbcbda459de96c
KimDoHyun1011 <hot99009@gmail.com>
2024-07-29 22:43:36 +0900

    블록체인 해시코드 작성 완료 및 추후 시스템 개편 작업예정

--- build/index.js
+++ build/index.js
@@ -1,13 +1,2 @@
-<<<<<<< HEAD
 "use strict";
-console.log("b");
-=======
-class Block {
-    constructor(data) {
-        this.data = data;
-    }
-    static hello() {
-        return "hi";
-    }
-}
->>>>>>> 88411d640e359c4e52256588272c3862dd9fe195
+console.log("hi");
